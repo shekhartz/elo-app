21fe142c62683790de84ebb39db402627d9baa91
shekhartz <hshekhar0@gmail.com>
2021-07-09 18:05:45 +0530

    commit

--- elo.py
+++ elo.py
@@ -8,9 +8,8 @@ import os
 
 st.title("ELO Loyalty Score Prediction")
 #--------------------------------------------------------------------------------------------------
-#DATA_URL = ('https://elo-stream.s3.us-east-2.amazonaws.com/train_FE2.csv')
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://elo-stream.s3.us-east-2.amazonaws.com/train_FE2.csv')
+
 
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
